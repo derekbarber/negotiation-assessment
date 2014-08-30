14574a683a20bebe57ef93c2ba8da673891661cb
Derek Barber <db@derekbarber.ca>
2014-08-29 17:06:48 -0700

    Add assessments controller

---
+++ app/controllers/assessments_controller.rb
@@ -0,0 +1,12 @@
+class AssessmentsController < ApplicationController
+
+  def index
+    @assessments = Assessment.all
+  end
+
+  def show
+    @assessment = Assessment.find(params[:id])
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :assessments, :only => [:index, :show]
+
   match '/about',    to: 'static_pages#about',    via: 'get'
   match '/terms-of-use',  as: "terms", to: 'static_pages#terms_of_use',  via: 'get'
   match '/legal-notice',  as: "legal_notice", to: 'static_pages#legal_notice',  via: 'get'
