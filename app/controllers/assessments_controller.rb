16492668a8ba20338e4a903915eb143daa6903dc
Derek Barber <db@derekbarber.ca>
2014-09-02 08:11:13 -0700

    Convert assessment to multi-page process with one question per page

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -6,7 +6,12 @@ class AssessmentsController < ApplicationController
 
   def show
     @assessment = Assessment.find(params[:id])
-  end
 
+    params.has_key?(:page) ? @page = params[:page].to_i : @page = 0
+
+    @progress = (@page.to_f / @assessment.questions.count) * 100 if @page > 0
+
+    @page > 0 ? @question = @assessment.questions.limit(1).offset(@page).first : @question = nil
+  end
 
 end
